5709cd5678431948cd74faf51714f14f724acb1b
KacobJeith <jacobkeith42@gmail.com>
2018-05-27 14:45:10 -0400

    HEEP-921 user input non-blocking read frequency, read analog moisture values, user set analog value threshold

--- SoilHygrometer/HeepDeviceDefinitions.h
+++ SoilHygrometer/HeepDeviceDefinitions.h
@@ -2,7 +2,7 @@
 #include "ESP8266_HeepComms.h"
 #include "ESP8266_NonVolatileMemory.h"
 #include "Arduino_Timer.h" 
-heepByte deviceIDByte [STANDARD_ID_SIZE] = {0x00, 0x00, 0x00, 0x00};
-uint8_t mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+heepByte deviceIDByte [STANDARD_ID_SIZE] = {0xe0, 0x0e,0xd0, 0x0d};
+uint8_t mac[6] = {0x0a, 0xb0, 0x0c, 0xd0, 0x0e, 0xf0};
 unsigned char clearMemory = 1;
 char* heepDeviceName = "SoilHygrometer";
--- SoilHygrometer/SoilHygrometer.ino
+++ SoilHygrometer/SoilHygrometer.ino
@@ -2,14 +2,34 @@
 #include "HeepDeviceDefinitions.h"
 
 #define WATERME_PIN 14
+#define ANALOG_PIN A0
+
+unsigned long previousMillis = 0;
 
 void InitializeControlHardware(){
   pinMode(WATERME_PIN,INPUT);
+  pinMode(ANALOG_PIN,INPUT);
 }
 
 int ReadWaterMe(){
   int currentSetting = digitalRead(WATERME_PIN);
-  SetControlValueByName("WaterMe",currentSetting);
+  SetControlValueByName("DigitalWaterMe",currentSetting);
+  return currentSetting;
+}
+
+int ReadAnalog(){
+  int currentSetting = analogRead(ANALOG_PIN) / 10;
+  SetControlValueByName("Moistness", currentSetting);
+
+  if (currentSetting >  GetControlValueByName("TriggerValue")) {
+    SetControlValueByName("AnalogWaterMe", true);
+  } else {
+    SetControlValueByName("AnalogWaterMe", false);
+  }
+  
+  Serial.print("Moistness: ");
+  Serial.println(currentSetting);
+  
   return currentSetting;
 }
 
@@ -18,14 +38,36 @@ void setup()
 
   Serial.begin(115200);
   InitializeControlHardware();
-  AddOnOffControl("WaterMe",HEEP_OUTPUT,0);
-  StartHeep(heepDeviceName, 2);
+
+  std::string SSID1 = "ssid";
+  std::string Password1 = "password";
+  AddWiFiSettingsToMemory(&SSID1[0], SSID1.length(), &Password1[0], Password1.length(), deviceIDByte, 0);
+  
+  AddOnOffControl("DigitalWaterMe",HEEP_OUTPUT,0);
+  AddOnOffControl("AnalogWaterMe",HEEP_OUTPUT,0);
+  AddRangeControl("Moistness",HEEP_OUTPUT, 100, 0, 0);
+  
+  AddRangeControl("TriggerValue",HEEP_INPUT, 100, 0, 70);
+  AddRangeControl("ReadFrequency",HEEP_INPUT, 100, 0, 3);
+  
+  StartHeep(heepDeviceName, 12);
 
 }
 
 void loop()
 {
   PerformHeepTasks();
-  ReadWaterMe();
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 1000 * GetControlValueByName("ReadFrequency")) {
+    previousMillis = currentMillis;
+
+    ReadWaterMe();
+    ReadAnalog();
+    
+  }
+  
+  
 
 }
